4ced49aea634d63284b7d6a239cb64e3def9e15c
philippbruhin <info@philippbruhin.ch>
2020-02-16 22:36:57 +0100

    1:20:04

---
+++ src/components/QuestionBox.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="question-box-container">
+    <b-jumbotron>
+
+      <template v-slot:lead>
+        {{ currentQuestion.question }}
+      </template>
+
+      <hr class="my-4">
+
+      <b-list-group>
+        <b-list-group-item
+          v-for="(answer, index) in answers"
+          :key="index"
+          @click.prevent="selectAnswer(index)"
+        >
+          {{ answer }}          
+        </b-list-group-item>
+      </b-list-group>
+
+      <b-button variant="primary" href="#">Submit</b-button>
+      <b-button @click="next" variant="success" href="#">
+        Next
+      </b-button>
+    </b-jumbotron>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    currentQuestion: Object,
+    next: Function
+  },
+  data: function() {
+    return {
+      selectedIndex: null,
+    }
+  },
+  computed: {
+    answers() {
+      let answers = [...this.currentQuestion.incorrect_answers]
+      answers.push(this.currentQuestion.correct_answer)
+      return answers
+    }
+  },
+  methods: {
+    selectAnswer(index) {
+      this.selectedIndex = index
+    },
+  }
+}
+</script>
+
+<style scoped>
+  .list-group {
+    margin-bottom: 15px;
+  }
+  .list-group-item {
+    margin-bottom: 5px;
+  }
+  .btn {
+    margin: 0 5px;
+  }
+</style>
